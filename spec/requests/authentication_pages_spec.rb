9da8bd0aca7675143766f6d59b51cf82cd07cfe0
Laurence E. Drake <laurencedrake@gmail.com>
2013-07-08 10:07:15 -0500

    9.3 Showing all users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,16 +35,20 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      # before do
+      #   fill_in "Email",    with: user.email.upcase
+      #   fill_in "Password", with: user.password
+      #   click_button "Sign in"
+      # end
+      before { sign_in user }
 
       it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users',    href: users_path) }
       it { should have_link('Profile',  href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
+      
       it { should_not have_link('Sign in', href: signin_path) }
 
       describe "followed by signout" do
@@ -74,7 +78,62 @@ describe "Authentication" do
           end
         end
       end
+      
+      describe "in the Users controller" do
+        
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+          it { should have_selector('div.alert.alert-notice') }
+        end
+
+        # describe "submitting to the update action" do
+        #   before { put user_path(user) }
+        #   specify { response.should redirect_to(signin_path) }
+        # end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        # describe "visiting the following page" do
+        #   before { visit following_user_path(user) }
+        #   it { should have_selector('title', text: 'Sign in') }
+        # end
+
+        # describe "visiting the followers page" do
+        #   before { visit followers_user_path(user) }
+        #   it { should have_selector('title', text: 'Sign in') }
+        # end
+      end
     end
+
+    # describe "in the Microposts controller" do
+
+    #   describe "submitting to the create action" do
+    #     before { post microposts_path }
+    #     specify { response.should redirect_to(signin_path)}
+    #   end
+
+    #   describe "submitting to the destroy action" do
+    #     before { delete micropost_path(FactoryGirl.create(:micropost)) }
+    #     specify { response.should redirect_to(signin_path) }
+    #   end
+    # end
+
+    # describe "in the Relationships controller" do
+      
+    #   describe "submitting to the create action" do
+    #     before { post relationships_path }
+    #     specify { response.should redirect_to(signin_path)}
+    #   end
+
+    #   describe "submitting to the destroy action" do
+    #     before { delete relationship_path(1) }
+    #     specify { response.should redirect_to(signin_path) }
+    #   end
+    # end
 
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
